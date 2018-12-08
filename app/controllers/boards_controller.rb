84d33a9712408bd29fa588acd97f7f963dbc0e48
James Walsh <jameswalsh@Jamess-MacBook-Pro-6.local>
2018-12-08 18:13:56 -0400

    Remove puts, Add request specs for when request is valid. TODO: Implement methods to check board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,16 @@
 class BoardsController < ApplicationController
   def is_valid
-    # TODO: add methods to determine if passed Sudoku is valid
-    render json: { is_valid: false }, status: :ok
+    puts '=-=-=-=-=-=-=-=-=-='
+    puts 'In is_valid_board'
+    puts params["board"].inspect
+    puts '=-=-=-=-=-=-=-=-=-='
+    render json: { is_valid: check_board(params["board"]) }, status: :ok
   end
+end
+
+private
+
+def check_board(board)
+  # TODO: Implement checking of board
+  false
 end
