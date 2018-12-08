f0ae9bb69777a0baa65d49c2fc9ae6e6da1e9eb3
James Walsh <jameswalsh@Jamess-MacBook-Pro-6.local>
2018-12-08 16:12:14 -0400

    Add constraints to value of a square

--- app/models/square.rb
+++ app/models/square.rb
@@ -3,7 +3,7 @@ class Square < ApplicationRecord
   belongs_to :board
 
   # Validation
-  validates :value, numericality: { only_integer: true }
+  validates :value, numericality: { only_integer: true, greater_than: 0, less_than: 10 }
   validates :row, numericality: { only_integer: true }
   validates :column, numericality: { only_integer: true }
   validates :block, numericality: { only_integer: true }
